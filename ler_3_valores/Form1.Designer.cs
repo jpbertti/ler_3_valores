b836c2cc63ac94fc4415291653624808244c3ef6
jpbertti <joaobertty@gmail.com>
2024-11-13 11:52:22 -0300

    Adicionar arquivos de projeto.

---
+++ ler_3_valores/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ler_3_valores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(47, 357);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(295, 366);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecionarei o menor número entra as opções";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(122, 142);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(59, 13);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = "Número 1: ";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(123, 207);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(59, 13);
+            this.lbl2.TabIndex = 4;
+            this.lbl2.Text = "Número 2: ";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(122, 280);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(59, 13);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.Text = "Número 3: ";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(187, 139);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 6;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(188, 200);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 7;
+            // 
+            // txtNumero3
+            // 
+            this.txtNumero3.Location = new System.Drawing.Point(187, 277);
+            this.txtNumero3.Name = "txtNumero3";
+            this.txtNumero3.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 409);
+            this.Controls.Add(this.txtNumero3);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero3;
+    }
+}
+
---
+++ ler_3_valores/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ler_3_valores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double numero1, numero2, numero3;
+
+            numero1 = double.Parse(txtNumero1.Text);
+            numero2 = double.Parse(txtNumero2.Text);
+            numero3 = double.Parse(txtNumero3.Text);
+
+            if (numero1 > numero2)
+            {
+                if (numero2 > numero3)
+                {
+                    MessageBox.Show("O número: " + numero1 + " é o maior de todos e o Menor é: " + numero3);
+                }
+                else
+                {
+                    MessageBox.Show("O número: " + numero1 + " é o maior de todos e o Menor é: " + numero2);
+                }
+            }
+
+            if (numero2 > numero1)
+            {
+                if (numero1 > numero3)
+                {
+                    MessageBox.Show("O número: " + numero2 + " é o maior de todos e o Menor é: " + numero3);
+                }
+                else
+                {
+                    MessageBox.Show("O número: " + numero2 + " é o maior de todos e o Menor é: " + numero1);
+                }
+            }
+
+            if (numero3 > numero2)
+            {
+                if (numero2 > numero1)
+                {
+                    MessageBox.Show("O número: " + numero3 + " é o maior de todos e o Menor é: " + numero1);
+                }
+                else
+                {
+                    MessageBox.Show("O número: " + numero3 + " é o maior de todos e o Menor é: " + numero2);
+                }
+            }
+        }
+    }
+}
